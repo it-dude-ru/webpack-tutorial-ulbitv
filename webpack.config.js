d99dfc75f2afcfd2ac03eea5ae87c74e9a38eaea
it-dude-ru <it-dude@ya.ru>
2023-12-07 17:48:24 +0300

    ts installed

--- webpack.config.js
+++ webpack.config.js
@@ -7,16 +7,28 @@ const webpack = require('webpack');
 module.exports = (env) => {  // env - объект с переменными окружения, которые передаются при запуске скрипта
 	return {				 // переменная задаётся в package.json в строках типа: "build:dev": "webpack --env mode=development"
 		mode: env.mode ?? 'development',
-		entry: path.resolve(__dirname, 'src', 'index.js'),
+		entry: path.resolve(__dirname, 'src', 'index.ts'),
 		output: {
 			path: path.resolve(__dirname, 'build'),
 			filename: '[name].[contenthash].js',
 			clean: true,
 		},
 		plugins: [
-			new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),
-			new webpack.ProgressPlugin(),
+			new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),  // Создаёт index.html по шаблону, подключает в него скрипты при сборке
+			new webpack.ProgressPlugin(), // Выводит проценты прогресса во время сборки.
 		],
+		module: {
+			rules: [
+				{
+					test: /\.tsx?$/,
+					use: 'ts-loader',
+					exclude: /node_modules/,
+				},
+			],
+		},
+		resolve: {
+			extensions: ['.tsx', '.ts', '.js'],
+		},
 	};
 };
 
